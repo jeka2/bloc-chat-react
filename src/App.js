72b549e23b43c0c5958c6b4e75fdad91c7c8271d
Yevgeniy Ivanov <anigeka@gmail.com>
2018-02-14 20:06:33 -0600

    checkpoint complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import RoomList from './components/RoomList';
 import './App.css';
+import * as firebase from 'firebase';
+
+  var config = {
+    apiKey: "AIzaSyDD9YkAgt9Q63JP_rhIoFbDYNqH86tQY6E",
+    authDomain: "bloc-chat-react-7ed45.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-7ed45.firebaseio.com",
+    projectId: "bloc-chat-react-7ed45",
+    storageBucket: "bloc-chat-react-7ed45.appspot.com",
+    messagingSenderId: "981516815952"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase}/>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+
+
+    this.state = {
+      rooms: [],
+
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+
+  }
+
+componentDidMount() {
+  this.roomsRef.on('child_added', snapshot => {
+    const room = snapshot.val();
+       room.key = snapshot.key;
+       this.setState({ rooms: this.state.rooms.concat( room ) })
+  });
+}
+
+render() {
+  /*this.state.rooms.map( (val,index) => {
+    console.log(val.name);
+  });*/
+    return (
+      <div className="room-list">
+        <ul>
+          {
+            this.state.rooms.map( (val,index) => {
+              return <li key={index}>{val.name}</li>
+
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default RoomList;
