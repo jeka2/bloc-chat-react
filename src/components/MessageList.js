cc3b5e6ff215b0973c9caa676e809553f62db13a
Yevgeniy Ivanov <anigeka@gmail.com>
2018-02-24 19:56:42 -0600

    sds

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -9,13 +9,11 @@ class MessageList extends Component {
 
     this.state = {
       message: [],
+      messagesToShow: []
     }
 
     this.messageRef = this.props.firebase.database().ref('credentials');
-    console.log(this.messageRef)
-    //this.messageRef.push({
-      //startedAt: this.firebase.database.ServerValue.TIMESTAMP
-    //})
+
   }
 
   componentDidMount() {
@@ -31,10 +29,15 @@ assignMessage (e) {
   e.preventDefault();
   if(this.newMessage.value === '') {return}
 
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date +' '+ time;
+
   var roomInfo = {
     userName: '',
     content: this.newMessage.value,
-    sentAt: /*this.messageRef.startedAt*/'',
+    sentAt: dateTime,
     roomId: this.props.keyId
   }
 
@@ -44,10 +47,25 @@ assignMessage (e) {
 
   this.setState({ message: this.state.message.concat( roomInfo )})
   this.newMessage.value = '';
-  console.log(this.state)
+  this.messageFilter();
+}
+
+messageFilter() {
+  var key = this.props.keyId;
+  var messages = this.state.message;
+  var filteredArray = fil(messages)
+  console.log(messages[0].roomId)
+
+  function fil(messages, key){
+    var arr = [];
+    for(let i = 0; i < messages.length; i++) {
+
+    }
+  }
 }
 
 render() {
+
   const isRoomChosen = this.props.keyId !== '';
   return(
   <div>
@@ -58,6 +76,11 @@ render() {
       <input type="submit" />
       </form>
     ) : (null)}
+    </div>
+    <div className="message-scroll">
+    <ul>
+
+    </ul>
     </div>
   </div>
   );
